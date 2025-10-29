29a23e0eb43517099dd48aae12754e6bde780192
berta <bert.soto@duocuc.cl>
2025-10-28 22:56:31 -0300

    RepareAndPers

--- app/src/main/java/com/example/charmeetchic_grupo2/navigation/Routes.kt
+++ app/src/main/java/com/example/charmeetchic_grupo2/navigation/Routes.kt
@@ -1,7 +1,20 @@
+package com.example.charmeetchic_grupo2.navigation
+
 sealed class Routes(val route: String) {
+    // 🏠 Sección principal
     object Home : Routes("home")
+
+    // 🧾 Autenticación
     object Login : Routes("login")
     object Register : Routes("register")
+    object Profile : Routes("profile")
+
+    // 💎 Tienda
     object Catalog : Routes("catalog")
     object Cart : Routes("cart")
+    object RepareAndPers : Routes("repare_and_pers")
+
+    // 📞 Información y contacto
+    object About : Routes("about")
+    object Contact : Routes("contact")
 }
